c88b43dd7bbd7dda63ce6fe2301232f24c038250
Coolkids <s196774065@gmail.com>
2025-10-26 09:51:14 +0800

    fix bug

--- app/utils/http_utils.py
+++ app/utils/http_utils.py
@@ -161,7 +161,7 @@ class RequestUtils:
                                     cookies=self._cookies,
                                     timeout=self._timeout,
                                     allow_redirects=allow_redirects)
-           if response.status_code >= 500:
+            if response.status_code >= 500:
                 response.raise_for_status()
             return response
         except requests.exceptions.RequestException as e:
