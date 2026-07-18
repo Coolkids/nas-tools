669085f0b86f240344ca39ca36ffb6ba77692028
Coolkid <s196774065@gmail.com>
2026-07-18 19:10:47 +0800

    修改Docker镜像

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 
-cd ${WORKDIR:-/app}
-
-echo "以 PUID=${PUID}，PGID=${PGID} 的身份启动程序..."
-mkdir -p /.local
-mkdir -p /.pm2
-chown -R "${PUID}":"${PGID}" "${WORKDIR}" /config /.local /.pm2 /var/lib/nginx /var/log/nginx
+cd ${WORKDIR:-/app/nas-tools}
 
 umask "${UMASK}"
 
-echo "启动 nginx..."
-nginx -g "daemon off;" &
-NGINX_PID=$!
-
-echo "启动 Flask 后端..."
-exec gosu "${PUID}":"${PGID}" "$(which dumb-init)" "$(which pm2-runtime)" start run.py -n NAStool --interpreter python3
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
